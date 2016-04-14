42d4e21b4712bb3d880340e775b875bacd4f23b4
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-14 09:16:15 -0400

    Connected to database

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+var mongoose    = require("mongoose");
+
+var ThingSchema = {
+  name:         String,
+  description:  String
+}
+
+mongoose.model("Thing", ThingSchema);
+mongoose.connect("mongodb://localhost/thingr");
